d285c5ba8437622ee68cd3259807768103ea04da
Alexandre Meira <22403726@me.uvsq.fr>
2026-04-03 15:50:57 +0200

    implémentation fonction test sur l'addition

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,9 +3,9 @@ import pycalc.pycalc as calculator
 
 class PyCalcUnitTests(unittest.TestCase):
   def test_add(self):
-    left = [5]
-    right = [9]
-    expected = [14]
+    left = [5,10,7,365,0,0,0,-50,-12,-4,32,4,-16,-7]
+    right = [9,5,0,-2,445,0,-54,100,0,-49,-32,-6,16,5]
+    expected = [14,15,7,363,445,0,-54,50,-12,-53,0,-2,0,-2]
     for i in range(len(left)):
       l = left[i]
       r = right[i]
